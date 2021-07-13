dd472ffac8b8ae18b3a8b3fac0ab3bed5177eac5
Borislav Ivanov <borislavivanovtw@abv.bg>
2021-07-13 04:19:33 +0300

    print articles plus effects

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -7,6 +7,11 @@ import firsticon from "../../assets/icon-online.svg";
 import secondicon from "../../assets/icon-budgeting.svg";
 import thirdicon from "../../assets/icon-onboarding.svg";
 import lasticon from "../../assets/icon-api.svg";
+import Article from "../../components/article.component/articles.component";
+import currency from "../../assets/image-currency.jpg";
+import restaurant from "../../assets/image-restaurant.jpg";
+import confetti from "../../assets/image-confetti.jpg";
+import plane from "../../assets/image-plane.jpg";
 const HomePage = () => {
   return (
     <div className="homepage container">
@@ -70,7 +75,32 @@ const HomePage = () => {
       </div>
       {/* Last Section  */}
       <h2 className="article-head">Latest Articles</h2>
-      <div className="d-flex"></div>
+      <div className="d-flex article-cont">
+        <Article
+          smalltext="By Claire Robinson"
+          header="Receive money in any currency with no fees"
+          description="The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …"
+          image={currency}
+        />
+        <Article
+          smalltext="By Wilson Hutton"
+          header="Treat yourself without worrying about money"
+          description="Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …"
+          image={restaurant}
+        />
+        <Article
+          smalltext="By Wilson Hutton"
+          header="Take your Easybank card wherever you go"
+          description="We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …"
+          image={confetti}
+        />
+        <Article
+          smalltext="By Claire Robinson"
+          header="Our invite-only Beta accounts are now live!"
+          description="After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ..."
+          image={plane}
+        />
+      </div>
     </div>
   );
 };
